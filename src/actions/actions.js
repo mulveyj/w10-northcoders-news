387661d6dc166513ff8e843b0b7318c9303f3cc5
jonsey247 <jacobfilmjones@hotmail.com>
2017-07-13 16:43:41 +0100

    types and actions files updated

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,1 +1,37 @@
-// import * as types from './types';
+import * as types from './types';
+import axios from  'axios';
+import {ROOT} from '../../config';
+
+export function fetchArticles () {
+    return function (dispatch) {
+        dispatch(fetchArticlesRequest());
+        axios.get(`${ROOT}/articles`)
+        .then(res => {
+            dispatch(fetchArticlesSuccess(res.data.articles));
+        })
+        .catch(err => {
+            dispatch(fetchArticlesError(err));
+        });
+    };
+}
+
+export function fetchArticlesRequest () {
+    return {
+        type: types.FETCH_ARTICLES_REQUESTS
+    };
+}
+
+export function fetchArticlesSuccess (articles) {
+    return {
+        type: types.FETCH_ARTICLES_SUCCESS,
+        payload: articles
+    };
+}
+
+export function fetchArticlesError (error) {
+    return {
+        type: types.FETCH_ARTICLES_ERROR,
+        payload: error
+    };
+}
+
