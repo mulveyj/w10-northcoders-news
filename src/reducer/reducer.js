1888b0db82fc1cb0dda3bf606b467d040e6f7b79
mulveyj <github@joemulvey.com>
2017-07-13 17:00:58 +0100

    reducer populated with data get actions

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,16 +1,37 @@
-// import * as types from '../actions/types';
+import * as types from '../actions/types';
 
 const initialState = {
   articles: [],
+  loading: false,
+  error: null,
   selectedTopic: null
 };
 
 function reducer (prevState = initialState, action) {
   if (!action) return prevState;
-  const newState = Object.assign({}, prevState);
 
+  if (action.type === types.FETCH_ARTICLES_REQUESTS) {
+    const newState = Object.assign({}, prevState);
+    newState.loading = true;
+    return newState;
+  }
 
-  return newState;
+  if (action.type === types.FETCH_ARTICLES_SUCCESS) {
+    const newState = Object.assign({}, prevState);
+    newState.articles = action.payload;
+    newState.loading = false;
+    return newState;
+  }
+
+  if (action.type === types.FETCH_ARTICLES_ERRORS) {
+    const newState = Object.assign({}, prevState);
+    newState.articles = [];
+    newState.loading = false;
+    newState.error = action.error;
+    return newState;
+  }
+
+  return prevState;
 }
 
 export default reducer;
